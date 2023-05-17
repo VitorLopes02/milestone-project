aea88c0364221dfd76e108dec39f94a57132e7d6
Vitor Lopes <vitinhoolopes.vl@gmail.com>
2023-05-17 11:17:04 -0300

    db orders

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const mongodb = require("mongodb");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 class User {
   constructor(email, password, fullname, street, postal, city) {
@@ -14,8 +15,17 @@ class User {
     };
   }
 
+  static async findById(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    return db
+      .getDb()
+      .collection("users")
+      .findOne({ _id: uid }, { projection: { password: 0 } });
+  }
+
   getUserWithSameEmail() {
-    return db.getDb().collection('users').findOne({ email: this.email });
+    return db.getDb().collection("users").findOne({ email: this.email });
   }
 
   async existsAlready() {
@@ -29,7 +39,7 @@ class User {
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
-    await db.getDb().collection('users').insertOne({
+    await db.getDb().collection("users").insertOne({
       email: this.email,
       password: hashedPassword,
       name: this.name,
