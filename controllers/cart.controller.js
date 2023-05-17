c6d480bf0413ed48071488318e36bc5e975d3c86
Vitor Lopes <vitinhoolopes.vl@gmail.com>
2023-05-17 12:05:48 -0300

    finished project

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,51 +1,51 @@
 const Product = require('../models/product.model');
 
-function getCart(req, res) {
-    res.render('customer/cart/cart');
+async function getCart(req, res) {
+  res.render('customer/cart/cart');
 }
 
 async function addCartItem(req, res, next) {
-    let product
-    try {
-        product = await Product.findById(req.body.productId);
-    } catch (error) {
-        next(error);
-        return;
-    }
-    
-    const cart = res.locals.cart;
+  let product;
+  try {
+    product = await Product.findById(req.body.productId);
+  } catch (error) {
+    next(error);
+    return;
+  }
 
-    cart.addItem(product);
-    req.session.cart = cart;
+  const cart = res.locals.cart;
 
-   res.status(201).json({
+  cart.addItem(product);
+  req.session.cart = cart;
+
+  res.status(201).json({
     message: 'Cart updated!',
-    newTotalItems: cart.totalQuantity
-   });  
-};
+    newTotalItems: cart.totalQuantity,
+  });
+}
 
 function updateCartItem(req, res) {
-    const cart = res.locals.cart;
-  
-    const updatedItemData = cart.updateItem(
-      req.body.productId,
-      req.body.quantity
-    );
-  
-    req.session.cart = cart;
-  
-    res.json({
-      message: 'Item updated!',
-      updatedCartData: {
-        newTotalQuantity: cart.totalQuantity,
-        newTotalPrice: cart.totalPrice,
-        updatedItemPrice: updatedItemData.updatedItemPrice,
-      },
-    });
-  }
-  
-  module.exports = {
-    addCartItem: addCartItem,
-    getCart: getCart,
-    updateCartItem: updateCartItem,
-  };
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    +req.body.quantity
+  );
+
+  req.session.cart = cart;
+
+  res.json({
+    message: 'Item updated!',
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
+
+module.exports = {
+  addCartItem: addCartItem,
+  getCart: getCart,
+  updateCartItem: updateCartItem,
+};
